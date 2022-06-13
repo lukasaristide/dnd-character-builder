8fb0736071b104174000c8b64daaef687dd6afaa
lukasaristide <luk2002luk@gmail.com>
2022-06-13 04:14:40 +0200

    slight refactor of import and deletion of DB

---
+++ app/src/main/java/com/dndcharacterbuilder/database/DBImportConsts.kt
@@ -0,0 +1,7 @@
+package com.dndcharacterbuilder.database
+
+object DBImportConsts {
+    const val INDEX_CLASSES_FILENAME = "index.json"
+    const val RACES_FILENAME = "races.json"
+    const val CLASS_DIR = "class/"
+}
